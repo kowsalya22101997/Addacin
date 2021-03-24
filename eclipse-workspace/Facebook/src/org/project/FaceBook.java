8efbabda0f111493921f2daf81fc1a22db7c9bcb
kowsalyaguru <kowsalyaguru22@gmail.com>
2021-03-24 14:54:54 +0530

    source

---
+++ eclipse-workspace/Facebook/src/org/project/FaceBook.java
@@ -0,0 +1,35 @@
+package org.project;
+
+public class FaceBook {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=10;
+		int c;
+		c=a+b;
+		System.out.println(c);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ eclipse-workspace/Instagaram/src/org/source/First.java
@@ -0,0 +1,13 @@
+package org.source;
+
+public class First {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=10;
+		int c;
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
